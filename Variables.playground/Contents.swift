72e86613dd82f6135376a616dfb5f13bab916ade
sm7urw <henrik@korslind.com>
2016-06-25 23:16:51 +0200

    Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground";
+
+var number = 20
+
+var decimal = 50.5
+
+decimal = 20.23
+
+let newDecimal = 40.5   // Konstant värde
+
+var welcomeMessage = "Hej, ditt konto..."
+
+var accountBalance = -500
+
+var account1 = 0.2, account2 = 0.4, account3 = 0.6
+
+var newMessage: String
+
+newMessage = 50         // Kan inte vara siffror eftersom var newMessage sattes till String.
+
+
+// Kommentar; bra kod dokumenterar sig själv så sätt inte kommentarer överallt. Men kan vara bra att kommentera kod som inte behövs eller buggar.
+
+/* Så här kan man också kommentera */
+
+// Swift använder inte semikolon. Swift läser raden och går sedan till nästa. Vill man ha flera statments på en och samma rad använder man semikolon.
